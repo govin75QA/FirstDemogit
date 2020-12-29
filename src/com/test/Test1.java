f23d8b44ce87f4275f981346c0b8964ff8e48f0c
GOmule <govind.mule75@gmail.com>
2020-12-29 11:21:02 +0530

    first commit

---
+++ src/com/test/Test1.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test1 {
+	static String hungry="Yes";
+
+	public static void main(String[] args) {
+		
+		if(hungry=="Yes") {
+			System.out.println("test pass");
+		}
+		else {
+			System.out.println("test fail");
+		}
+	}
+
+}
